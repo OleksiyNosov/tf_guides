f5138ac4de42baf1908ed4761f1416c2349508fb
Oleksiy Nosov <nosov@mlsdev.com>
2018-04-12 15:17:08 +0300

    Finish work on cnn_mnist.py

--- project/tutorial_on_layers/cnn_mnist.py
+++ project/tutorial_on_layers/cnn_mnist.py
@@ -10,16 +10,13 @@ import tensorflow as tf
 
 tf.logging.set_verbosity(tf.logging.INFO)
 
-if __name__ = "__main__":
-  tf.app.run()
-
 def cnn_model_fn(features, labels, mode):
   """Model function for CNN."""
   # Input Layer
   input_layer = tf.reshape(features['x'], [-1, 28, 28, 1])
 
   # Convolutional Layer #1
-  conv1 = tf.layer.conv2d(
+  conv1 = tf.layers.conv2d(
     inputs = input_layer,
     filters = 32,
     kernel_size = [5, 5],
@@ -41,15 +38,88 @@ def cnn_model_fn(features, labels, mode):
   # Dense Layer
   pool2_flat = tf.reshape(pool2, [-1, 7 * 7 * 64])
   dense = tf.layers.dense(inputs = pool2_flat, units = 1024, activation = tf.nn.relu)
-  dropout = tf.layers.dropout(
-    inputs = dense, rate = 0.4, training = mode == tf.estimator.ModeKeys.TRAIN)
+  dropout = tf.layers.dropout(inputs = dense, rate = 0.4, training = mode == tf.estimator.ModeKeys.TRAIN)
 
   # Logits Layer
   logits = tf.layers.dense(inputs = dropout, units = 10)
 
   predictions = {
     # Generate predictions (for PREDICT and EVAL mode)
-    'classes': tf.argmax(inputs  = logits, axis = 1)
+    'classes': tf.argmax(input  = logits, axis = 1),
     # Add `softmax_tensor` to the graph. It is used for PREDICT and by the `logging_hook`
     'probabilities': tf.nn.softmax(logits, name = 'softmax_tensor')
   }
+
+  if mode == tf.estimator.ModeKeys.PREDICT:
+    return tf.estimator.EstimatorSpec(mode = mode, predictions = predictions)
+
+  # Calculate Loss (for both TRAIN and EVAL modes)
+  loss = tf.losses.sparse_softmax_cross_entropy(labels = labels, logits = logits)
+
+  if mode == tf.estimator.ModeKeys.TRAIN:
+    optimizer = tf.train.GradientDescentOptimizer(learning_rate = 0.001)
+    train_op = optimizer.minimize(loss = loss, global_step = tf.train.get_global_step())
+    return tf.estimator.EstimatorSpec(mode = mode, loss = loss, train_op = train_op)
+
+  # Add evaluation metrics (for EVAL mode)
+  eval_metric_ops = {'accuracy': tf.metrics.accuracy(labels = labels, predictions = predictions['classes'])}
+
+  return tf.estimator.EstimatorSpec(mode = mode, loss = loss, eval_metric_ops = eval_metric_ops)
+
+def main(unused_argv):
+  # Load training and eval data
+  mnist = tf.contrib.learn.datasets.load_dataset('mnist')
+
+  train_data = mnist.train.images # Returns np.array
+  train_labels = np.asarray(mnist.train.labels, dtype = np.int32)
+
+  eval_data = mnist.test.images # Returns np.array
+  eval_labels = np.asarray(mnist.test.labels, dtype = np.int32)
+
+  # Create the estimator
+  mnist_classifier = tf.estimator.Estimator(model_fn = cnn_model_fn, model_dir = '/tmp/mnist_convnet_model')
+
+  # Set up logging for predictions
+  tensors_to_log = {'probabilities': 'softmax_tensor'}
+  logging_hook = tf.train.LoggingTensorHook(tensors = tensors_to_log, every_n_iter = 50)
+
+  # Train the model
+  train_input_fn = tf.estimator.inputs.numpy_input_fn(
+    x = {'x': train_data},
+    y = train_labels,
+    num_epochs = None,
+    batch_size = 100,
+    shuffle = True)
+
+  mnist_classifier.train(
+    input_fn = train_input_fn,
+    steps = 2000,
+    hooks = [logging_hook])
+
+  # Evaluate model and print results
+  eval_input_fn = tf.estimator.inputs.numpy_input_fn(
+    x = {'x': eval_data},
+    y = eval_labels,
+    num_epochs = 1,
+    shuffle = False)
+
+  eval_results = mnist_classifier.evaluate(input_fn = eval_input_fn)
+  print(eval_results)
+
+if __name__ == '__main__':
+  tf.app.run()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
