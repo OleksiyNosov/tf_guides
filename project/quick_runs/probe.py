8b28a7c684a3bd50532b593961053998d82287fb
Oleksiy Nosov <nosov@mlsdev.com>
2018-04-12 15:17:55 +0300

    Minor changes

--- project/quick_runs/probe.py
+++ project/quick_runs/probe.py
@@ -1,6 +1,26 @@
+# Just disables the warning, doesn't enable AVX/FMA
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
 import tensorflow as tf
 
-foo = tf.constant([1,2,3,4,5,6])
+def puts(value):
+  print(value.eval())
+  print()
 
-sess = tf.Session()
-print(sess.run(foo[2:-2]))
+with tf.Session() as sess:
+  sess.run(tf.global_variables_initializer())
+
+  foo = tf.constant([[71, 72, 73, 74, 75, 76], [11, 13, 17, 20, 21, 30]])
+
+  puts(foo)
+
+  puts(tf.reshape(foo, [-1]))
+
+  puts(tf.reshape(foo, [6, 2]))
+
+  puts(tf.reshape(foo, [3, 4]))
+
+  puts(tf.argmax(input = tf.reshape(foo, [-1]), axis = 0))
+
+  puts(tf.argmax(input = foo, axis = 1))
